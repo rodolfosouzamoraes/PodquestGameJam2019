bb5f8f71d1c31bdf564011dcf6ffa505e36e7777
Fábio Lamin <fabio_lamin@hotmail.com>
2019-09-15 11:37:16 -0300

    Implementando a mecânica de Drag and Drop

---
+++ Assets/_Scripts/Drag and Drop/DragAndDrop.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragAndDrop : MonoBehaviour
+{
+    Vector3 dist;
+    Vector3 startPos;
+    float posX;
+    float posZ;
+    float posY;
+    Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.useGravity = false;
+    }
+    private void OnMouseUp()
+    {
+        rb.useGravity = true;
+    }
+    void OnMouseDown()
+    {
+        startPos = transform.position;
+        dist = Camera.main.WorldToScreenPoint(transform.position);
+        posX = Input.mousePosition.x - dist.x;
+        posY = Input.mousePosition.y - dist.y;
+        posZ = Input.mousePosition.z - dist.z;
+    }
+
+    void OnMouseDrag()
+    {
+        float disX = Input.mousePosition.x - posX;
+        float disY = Input.mousePosition.y - posY;
+        float disZ = Input.mousePosition.z - posZ;
+        Vector3 lastPos = Camera.main.ScreenToWorldPoint(new Vector3(disX, disY, disZ));
+        transform.position = new Vector3(lastPos.x, startPos.y, lastPos.z);
+    }
+}
