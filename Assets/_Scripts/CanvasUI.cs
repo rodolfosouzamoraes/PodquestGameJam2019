be75938e58e53723f0f305a6f9e0030d48044a9d
Rodolfo <rodolfomsouza@gmail.com>
2019-09-16 14:12:43 -0300

    UI Menu Principal

---
+++ Assets/_Scripts/CanvasUI.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasUI : MonoBehaviour
+{
+
+    public void SelectButton(Animator animator)
+    {
+        animator.SetBool("Selected", true);
+    }
+
+    public void NotSelectButton(Animator animator)
+    {
+        animator.SetBool("Selected", false);
+    }
+
+    public void Play(Animator camAnimator)
+    {
+        camAnimator.SetBool("PositionCamera", true);
+        this.gameObject.SetActive(false);
+    }
+
+    public void Manual()
+    {
+
+    }
+
+    public void Credits()
+    {
+
+    }
+
+    public void Sair()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/_Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] Animator animatorCamera;
+    [SerializeField] GameObject canvasUI;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MainMenu();
+    }
+
+    private void MainMenu()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            animatorCamera.SetBool("PositionCamera", false);
+            canvasUI.SetActive(true);
+        }
+    }
+}
