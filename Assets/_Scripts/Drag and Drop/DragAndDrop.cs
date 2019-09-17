c0dc78b0d636cf11bc53e0b17c40fa6bdf296999
Rodolfo <rodolfomsouza@gmail.com>
2019-09-17 11:22:37 -0300

    Posição dos alimentos no prato
    Criado a posição no prato e a validação do acerto ou erro do alimento. Modificado o script Drag and Drop para que o alimento comece na tabua ao invés de começar "voando" na fase. Add sons de acerto e erro.

--- Assets/_Scripts/Drag and Drop/DragAndDrop.cs
+++ Assets/_Scripts/Drag and Drop/DragAndDrop.cs
@@ -7,45 +7,74 @@ public class DragAndDrop : MonoBehaviour
     Vector3 dist;
     Vector3 startPos;
     Rigidbody rb;
-    AudioSource soundHit;
+    AudioSource[] soundHit; // Array para diversos sons que possa acontecer com o objeto
     float posX;
     float posZ;
     float posY;
     bool isDragging = false;
-    private void Awake()
+
+    private void Start()
     {
+        soundHit = GetComponents<AudioSource>();
         rb = GetComponent<Rigidbody>();
-        rb.useGravity = false;
-        isDragging = true;
-        soundHit = GetComponent<AudioSource>();
     }
+
     private void OnMouseUp()
     {
         rb.useGravity = true;
         isDragging = false;
-
-        soundHit.enabled = false;
     }
     void OnMouseDown()
     {
-        startPos = transform.position;
+        rb.useGravity = false;
+        isDragging = true;
+        transform.position = new Vector3(transform.position.x, transform.position.y + 4.5f, transform.position.z);
+        startPos = new Vector3(transform.position.x, transform.position.y, transform.position.z);
         dist = Camera.main.WorldToScreenPoint(transform.position);
         posX = Input.mousePosition.x - dist.x;
         posY = Input.mousePosition.y - dist.y;
         posZ = Input.mousePosition.z - dist.z;
-
-        soundHit.Play();
+        soundHit[0].Play();
     }
 
     void OnMouseDrag()
     {
-        if(isDragging)
+        if (isDragging)
         {
             float disX = Input.mousePosition.x - posX;
             float disY = Input.mousePosition.y - posY;
             float disZ = Input.mousePosition.z - posZ;
             Vector3 lastPos = Camera.main.ScreenToWorldPoint(new Vector3(disX, disY, disZ));
             transform.position = new Vector3(lastPos.x, startPos.y, lastPos.z);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        string objectTag = other.gameObject.tag;
+        switch (gameObject.tag)
+        {
+            case "Hamburger":
+                CheckTarget(objectTag, "PosiHamburger");
+                break;
+            case "Egg":
+                CheckTarget(objectTag, "PosiEgg");
+                break;
+        }
+    }
+
+    private void CheckTarget(string objectTag, string posiFood)
+    {
+        Destroy(GetComponent<DragAndDrop>()); // vai destruir o Script para não correr o risco do jogador pega-lo novamente
+        if (objectTag.Equals(posiFood))
+        {
+            soundHit[1].Play();
+            Debug.Log("Acertou!");
+        }
+        else
+        {
+            soundHit[2].Play();
+            Debug.Log("Errou!");
         }
     }
 }
