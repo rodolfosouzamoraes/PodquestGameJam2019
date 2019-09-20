4acdb052b72ddf7c55e819c5f1d733628253f00e
Fábio Lamin <fabio_lamin@hotmail.com>
2019-09-19 23:45:27 -0300

    Começando criação do cronômetro

---
+++ Assets/_Scripts/Cronometro/Timer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    //Variáveis
+    [SerializeField]
+    private int minute;
+    [SerializeField]
+    private int second;
+
+    //UI
+    [SerializeField]
+    private Text minuteText;
+    [SerializeField]
+    private Text secondText;
+
+
+
+}
